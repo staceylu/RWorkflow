7afc520abd3b685bc0bbc6cc512944f2e3fa7d89
staceylu <contact@stacey-williams.com>
2017-04-04 16:43:14 -0500

    added external javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+function isItChecked() {
+    if document.getElementById("item1").checked = true{
+      document.getElementByClass("strikethrough1").style.text-decoration = "line-through";
+    }
+    if document.getElementById("item2").checked = true{
+      document.getElementByClass("strikethrough2").style.text-decoration = "line-through";
+    }
+    if document.getElementById("item3").checked = true{
+      document.getElementByClass("strikethrough3").style.text-decoration = "line-through";
+    }
+}
+
+isItChecked;
